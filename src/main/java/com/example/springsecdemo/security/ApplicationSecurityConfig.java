14f16ca5c19af61bcbeef5317819a2835cb0852b
stevematdavies <stevematdavies@gmail.com>
2022-10-21 09:37:59 +0300

    Adding an in-memory test user

--- src/main/java/com/example/springsecdemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecdemo/security/ApplicationSecurityConfig.java
@@ -1,14 +1,28 @@
 package com.example.springsecdemo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfig  {
+public class ApplicationSecurityConfig {
+
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,5 +35,19 @@ public class ApplicationSecurityConfig  {
                 .httpBasic();
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+
+
 
 }
